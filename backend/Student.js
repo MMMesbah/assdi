eaeef88bccdb4b59a10135f68f662f3d5d73decb
MMMesbah <181493833+MMMesbah@users.noreply.github.com>
2025-06-25 06:58:59 +0600

    Update Student.js

--- backend/Student.js
+++ backend/Student.js
@@ -1,1 +1,10 @@
+const mongoose = require('mongoose');
+
+const studentSchema = new mongoose.Schema({
+  name: String,
+  roll: String,
+  class: String
+});
+
+module.exports = mongoose.model('Student', studentSchema);
 
